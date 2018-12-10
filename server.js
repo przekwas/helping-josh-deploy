f133e4156e6f56c90d1d8746e659d94aa6bf071c
przekwas <luke@covalence.io>
2018-12-10 16:39:12 -0600

    adding env var for port number

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000!");
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
 });
